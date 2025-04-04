972be436ea01db31f682e8a5be5c1b74d7fbd0d7
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2025-04-03 22:16:42 -0700

    Add images to admin dashboard.

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable, Image } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { router } from 'expo-router';
 
@@ -46,6 +46,48 @@ export default function AdminDashboard() {
           <Ionicons name="chevron-forward-outline" size={28} color="#a1a1aa" />
         </Pressable>
       ))}
+
+      <View className="mt-8 gap-5 rounded-xl bg-black p-4">
+        <Text className="mb-4 text-xl font-bold text-white">Lifetime Highlights</Text>
+
+        <View className="mb-4 flex-row justify-around gap-5">
+          <View className="items-center">
+            <Image
+              source={require('~/assets/novoarelayroad.jpg')}
+              className="h-32 w-48 rounded-lg"
+              resizeMode="cover"
+            />
+            <Text className="mt-2 text-lg font-semibold text-white">Total Trips</Text>
+          </View>
+          <View className="items-center">
+            <Image
+              source={require('~/assets/novoarelaytasks.jpg')}
+              className="h-32 w-48 rounded-lg"
+              resizeMode="cover"
+            />
+            <Text className="mt-2 text-lg font-semibold text-white">Assigned Trips</Text>
+          </View>
+        </View>
+
+        <View className="flex-row justify-around gap-5">
+          <View className="items-center">
+            <Image
+              source={require('~/assets/novoarelaymaintenance.jpg')}
+              className="h-32 w-48 rounded-lg"
+              resizeMode="cover"
+            />
+            <Text className="mt-2 text-lg font-semibold text-white">Maintenance Requests</Text>
+          </View>
+          <View className="items-center">
+            <Image
+              source={require('~/assets/novoarelaycity.jpg')}
+              className="h-32 w-48 rounded-lg"
+              resizeMode="cover"
+            />
+            <Text className="mt-2 text-lg font-semibold text-white">Client Cities</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
