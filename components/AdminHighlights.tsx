f581a01dee99d2e43e185bed4c9c8dc273e512ca
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2025-04-06 22:20:24 -0700

    Create custom hooks for moduralization.

--- components/AdminHighlights.tsx
+++ components/AdminHighlights.tsx
@@ -1,61 +1,62 @@
 import HighlightCard from './HighlightCard';
 import { View } from 'react-native';
+import { useCompanyTrips } from '~/hooks/useCompanyTrips';
+import { useMyTrips } from '~/hooks/useMyTrips';
+import { useAdminClientCount } from '~/hooks/useAdminClientCount';
+import useEmployeeCount from '~/hooks/useEmployeeCount';
+import useTruckCount from '~/hooks/useTruckCount';
+import useJoinDate from '~/hooks/useJoinDate';
 
 export default function AdminHighlights() {
+  const { tripCount, loading } = useCompanyTrips();
+  const { myTripCount } = useMyTrips();
+  const { clientCount } = useAdminClientCount();
+  const { employeeCount } = useEmployeeCount();
+  const { truckCount } = useTruckCount();
+  const { createdAt } = useJoinDate();
+
   return (
     <View className="flex-row flex-wrap justify-around gap-4 bg-black ">
-      <HighlightCard title="My Trips" icon={require('~/assets/novoarelayroad.jpg')} value="3" />
+      <HighlightCard
+        title="My Trips"
+        icon={require('~/assets/novoarelayroad.jpg')}
+        value={myTripCount}
+      />
 
       <HighlightCard
         title="Journey with NovoaRelay"
         icon={require('~/assets/novoarelaytasks.jpg')}
-        value="3"
+        value={createdAt}
       />
 
       <HighlightCard
         title="Total Employees"
         icon={require('~/assets/novoarelaymaintenance.jpg')}
-        value="3"
-      />
-
-      <HighlightCard
-        title="Regions Covered"
-        icon={require('~/assets/novoarelaycity.jpg')}
-        value="3"
+        value={employeeCount}
       />
 
       <HighlightCard
         title="Total Company Trips"
         icon={require('~/assets/novoarelaytasks.jpg')}
-        value="3"
+        value={loading ? '...' : tripCount.toString()}
       />
 
       <HighlightCard
         title="Fleet Size"
         icon={require('~/assets/novoarelaymaintenance.jpg')}
-        value="3"
+        value={truckCount}
       />
 
       <HighlightCard
-        title="Trips Dispatched"
-        icon={require('~/assets/novoarelaycity.jpg')}
-        value="3"
-      />
-      <HighlightCard
         title="Maintenance Logs"
         icon={require('~/assets/novoarelaymaintenance.jpg')}
-        value="3"
+        value="0"
       />
 
       <HighlightCard
         title="Client Accounts"
         icon={require('~/assets/novoarelaycity.jpg')}
-        value="3"
-      />
-      <HighlightCard
-        title="Completed Jobs"
-        icon={require('~/assets/novoarelaymaintenance.jpg')}
-        value="3"
+        value={clientCount}
       />
     </View>
   );
--- components/EmployeeHighlights.tsx
+++ components/EmployeeHighlights.tsx
@@ -1,28 +1,33 @@
+import { useMyTrips } from '~/hooks/useMyTrips';
 import HighlightCard from './HighlightCard';
 import { View } from 'react-native';
+import useJoinDate from '~/hooks/useJoinDate';
 
 export default function EmployeeHighlights() {
+  const { myTripCount } = useMyTrips();
+  const { createdAt } = useJoinDate();
+
   return (
     <View className="flex-row flex-wrap justify-around gap-4 bg-black ">
-      <HighlightCard title="Total Trips" icon={require('~/assets/novoarelayroad.jpg')} value="3" />
+      <HighlightCard
+        title="Total Trips"
+        icon={require('~/assets/novoarelayroad.jpg')}
+        value={myTripCount}
+      />
 
       <HighlightCard
         title="Journey with NovoaRelay"
         icon={require('~/assets/novoarelaytasks.jpg')}
-        value="3"
+        value={createdAt}
       />
 
       <HighlightCard
         title="Vehicles Towed"
         icon={require('~/assets/novoarelaymaintenance.jpg')}
-        value="3"
+        value={myTripCount}
       />
 
-      <HighlightCard
-        title="Cities Visited"
-        icon={require('~/assets/novoarelaycity.jpg')}
-        value="3"
-      />
+      <HighlightCard title="Logs Issued" icon={require('~/assets/novoarelaycity.jpg')} value="3" />
     </View>
   );
 }
